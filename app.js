a04fd65bb789e683b1a54973353a714673e11def
quoctrang12 <waipayttt@gmail.com>
2022-09-22 10:19:04 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.use(express.json());
 
 app.use('/api/contacts',contactsRouter);
 
-// app.use((req,res,next)=>{
-//     return next(new ApiError(404, "Resource not found"));
-// })
+app.use((req,res,next)=>{
+    return next(new ApiError(404, "Resource not found"));
+})
 
-// app.use((err,req,res,next)=>{
-//     return res.status(err.statusCode||500).json({message: err.message||"internal server Error"})
-// })
+app.use((err,req,res,next)=>{
+    return res.status(err.statusCode||500).json({message: err.message||"internal server Error"})
+})
 
 
 module.exports = app;
--- app/api-error.js
+++ app/api-error.js
@@ -6,4 +6,4 @@ class ApiError extends Error {
     }
 }
 
-module.exports = ApiError;
+module.exports = ApiError;  
